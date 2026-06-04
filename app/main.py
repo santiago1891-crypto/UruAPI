680694d57ddbc8fd1e378e2ba1f411578abd2993
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-04 19:57:51 -0300

    feat(app): register boleto router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from app.routers import utils_router, dolar_router
+from app.routers import boleto_router, dolar_router, utils_router
 
 app = FastAPI()
 
 app.include_router(router=utils_router, prefix="/utils")
 app.include_router(router=dolar_router, prefix="/dolar")
+app.include_router(router=boleto_router, prefix="/boleto")
 
 
 @app.get("/")
