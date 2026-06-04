96d45ba573bc41ba9da1dfc87349b2d6240b352e
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-04 19:57:52 -0300

    feat(utils): export generic scraper helpers

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,2 @@
 from .dependencies import HttpxClientDep
-from .scrapper import get_div_element, get_page_html, get_span_element
+from .scrapper import extract_price, extract_text, get_div_element, get_element_text, get_page_html, get_span_element
