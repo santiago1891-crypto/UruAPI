e5143b7475161b447c75324f24777d908c48aad1
Santiago-Nolla1891 <208549987+santiago1891-crypto@users.noreply.github.com>
2026-06-06 19:03:32 -0300

    real issues

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from app.routers import boleto_router, dolar_router, utils_router
+from app.routers import boleto_router, real_router, dolar_router, utils_router
 
 app = FastAPI()
 
 app.include_router(router=utils_router, prefix="/utils")
 app.include_router(router=dolar_router, prefix="/dolar")
+app.include_router(router=real_router, prefix="/real")
 app.include_router(router=boleto_router, prefix="/boleto")
 
 
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,4 @@
 from .utils import utils_router
 from .boleto import boleto_router
 from .dolar import dolar_router
+from .real import real_router
